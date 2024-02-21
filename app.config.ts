7d5140d6ce07096b18386655655bd89787c7123d
Yannick Jost <yannick@yjost.com>
2024-02-21 21:18:45 +0100

    First import

---
+++ app.config.ts
@@ -0,0 +1,24 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Hack Le Chalet',
+    description: '28-30 juin 2024 - Two days of hacking, fun and discussion in Alsace 🥨',
+    socials: {
+      twitter: 'AlsaceDigitale',
+    },
+    aside: {
+      level: 0,
+      collapsed: false,
+      exclude: []
+    },
+    main: {
+      padded: true,
+      fluid: true
+    },
+    header: {
+      logo: true,
+      showLinkIcon: true,
+      exclude: [],
+      fluid: true
+    }
+  }
+})
---
+++ components/Logo.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <img width="120" src="/HackLeChalet-logo.png" alt="Logo">
+  </div>
+</template>
+<style scoped>
+  img {
+    padding-top: 10px;
+  }
+</style>
