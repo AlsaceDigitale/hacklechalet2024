2fe117148a9b414407512d6891af288486f49c2e
Yannick Jost <6668+yanjost@users.noreply.github.com>
2024-04-16 23:59:20 +0200

    fix/update content (#6)
    * update schedule
    * add challenges
    * update content

---
+++ components/content/Challenge.vue
@@ -0,0 +1,17 @@
+<template>
+  <span class="dimmed-text">
+    <slot />
+  </span>
+</template>
+
+<script>
+export default {
+    name: 'Challenge'
+}
+</script>
+
+<style scoped>
+.dimmed-text {
+    opacity: 0.1; /* Adjust the opacity value to control the dimming effect */
+}
+</style>
